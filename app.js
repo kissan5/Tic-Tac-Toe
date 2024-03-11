3ef7b3f6a4c11164c2232a61c05cc6434055cba2
Kishan <kissanshaa@gmail.com>
2024-03-11 13:53:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Define variables
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
-let newGameBtn = document.querySelector("#new-btn");
+let newGameBtn = document.querySelector("#newGame-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 
